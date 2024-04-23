ff4c3d7c743addfa6acda2a48acc81c30ff8d21c
timoleppanen85 <31985525+timoleppanen85@users.noreply.github.com>
2024-04-23 19:19:14 +0300

    fixes

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ from user.routes import user_bp
 from datetime import timedelta
 
 # App settings
-app = Flask(__name__, static_folder="public", static_url_path="")
+app = Flask(__name__, static_folder="/public", static_url_path="/public")
 app.register_blueprint(user_bp)
 app.secret_key = os.environ.get("LOCALFLIX_SECRET")
 app.permanent_session_lifetime = timedelta(days=30)
@@ -39,7 +39,7 @@ def login_required(f):
 @app.route("/")
 @login_required
 def index():
-    return send_file("public\\index.html")
+    return send_file("index.html")
 
 
 @app.route("/api")
